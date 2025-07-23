27fd8c1075d402ec5bc425ab6881938656834b37
JRamses11 <ramchu_aldama@hotmail.com>
2025-07-22 20:20:09 -0700

    Comandos basicos de Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-FirstSteps' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-FirstSteps');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-  <h1>Hola Angular desde {{city.toLocaleUpperCase()}}</h1>
-  `,
+  imports: [UserComponent],
+  templateUrl: './app.component.html',
   styles: [
     `
     h1{
